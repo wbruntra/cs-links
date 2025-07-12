1488bd700cd6e627732d6dc2d85902055a8ca7b8
William Bruntrager <w.bruntrager@gmail.com>
2025-07-12 13:50:18 +0200

    Display db config

--- db.js
+++ db.js
@@ -1,6 +1,10 @@
 require('dotenv').config()
 const knexfile = require('./knexfile')
 console.log(`NODE_ENV = ${process.env.NODE_ENV}`)
-const knex = require('knex')(knexfile[process.env.NODE_ENV])
+const config = knexfile[process.env.NODE_ENV]
+
+console.log(`Using knex config: ${JSON.stringify(config, null, 2)}`)
+
+const knex = require('knex')(config)
 
 module.exports = knex
